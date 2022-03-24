a9d3db6e345a915712c871c1107bde8953b9ec9b
Bill Ni <bill.8ni@gmail.com>
2022-03-24 17:52:50 -0400

    rename project

---
+++ Captura/CapturaApp.swift
@@ -0,0 +1,26 @@
+//
+//  CapturaApp.swift
+//  Captura
+//
+//  Created by Bill Ni on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct CapturaApp: App {
+    
+    @NSApplicationDelegateAdaptor var delegate: AppDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                
+            }
+        }
+    }
+    
+    init() {
+        
+    }
+}
